5aee89cfa01dd59e68c53a9ff25220de1f1136b7
Diego Diniz <diegodinizz@gmail.com>
2021-09-20 13:05:32 +0100

    added favico

--- src/containers/NotesList.js
+++ src/containers/NotesList.js
@@ -16,15 +16,34 @@ const Title = styled.h1`
   margin: 40px 0;
 `
 
+const NoNotes = styled.div`
+  text-align: center;
+  font-size: 1.5em;
+  font-weight: 300;
+`
+
+const NotesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+
 export const NotesList = () => {
   const notes = useSelector(state => state.notes)
 
   return (
     <Container>
       <Title>Notes List</Title>
-      {notes.map(note => (
-        <Note key={note.id} details={note} />
-      ))}
+      {!notes.length ? (
+        <NoNotes>No notes</NoNotes>
+      ) : (
+        <NotesContainer>
+          {notes.map(note => (
+            <Note key={note.id} details={note} />
+          ))}
+        </NotesContainer>
+      )}
     </Container>
   )
 }
